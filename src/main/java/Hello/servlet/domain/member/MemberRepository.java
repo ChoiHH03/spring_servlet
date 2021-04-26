d97999a3f380484e3d75c5e608346b67bc8940fa
ChoiHH <choihh0316@gmail.com>
2021-04-26 10:08:00 +0900

    서블릿, JSP, MVC 패턴

---
+++ src/main/java/Hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,39 @@
+package Hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;  // 공통으로 사용되는 변수이므로 static 사용
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
